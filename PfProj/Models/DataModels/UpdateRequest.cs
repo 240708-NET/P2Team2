f2acaa0e2c0ed7d969821c1318b9f89216331067
ColtonXan <coltonxtownsend@gmail.com>
2024-08-02 16:57:08 -0500

    Update UpdateRequest.cs

--- PfProj/Models/DataModels/UpdateRequest.cs
+++ PfProj/Models/DataModels/UpdateRequest.cs
@@ -5,7 +5,7 @@ using PfProj.Entities;
 
 public class UpdateRequest
 { 
-    // May not work, I will figure this out -C
+    // May not work, I will figure this out. In the meantime rely on POST/GET to test your functionality -C
     public required string RequiredInputVar { get; set; }
     public int? OptionalInputVar{ get; set; }
     
