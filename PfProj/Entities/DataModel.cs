51189d8bcf3f1d66b7114f2a6db11e8a0af99432
ColtonXan <coltonxtownsend@gmail.com>
2024-08-02 16:49:37 -0500

    Update DataModel.cs

--- PfProj/Entities/DataModel.cs
+++ PfProj/Entities/DataModel.cs
@@ -6,13 +6,9 @@ public class DataModel
 {
     public int Id { get; set; }
     // Required Input
-    public required string FilePath { get; set; }
-    public required string TestingColName { get; set; }
-	public required string TargetColName { get; set; }
+    public required string RequiredInputVar { get; set; }
     // Optional Input
-    public int? ObservationLimit { get; set; }
-    // Output
-	public int? NumObservations { get; set; }
-    public double? Covar {get; set; }
-    public double? Cor {get; set; }
+    public int? OptionalInputVar { get; set; }
+    // Output (Looks the same as optional inputs here, but we distinquish them by omitting output variables from our request models found in Models/.)
+	public int? OutputVar { get; set; }
 }
