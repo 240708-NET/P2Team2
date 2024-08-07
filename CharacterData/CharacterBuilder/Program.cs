a6d00df1098f3e4a197671272dc3a45b22155fd0
ThyPirateKing <pirateking_nick@yahoo.com>
2024-08-07 16:22:16 -0400

    Created a loop to generate random items.

--- CharacterData/CharacterBuilder/Program.cs
+++ CharacterData/CharacterBuilder/Program.cs
@@ -35,8 +35,20 @@ namespace CharacterBuilder
             string path = @"./Characters.txt";
 
             string filePath = @"./ItemBuildList.txt";
-
-            SaveAllItems(CreateItems(5, 3, "Greatsword", filePath, "melee weapon", 10, "two handed", "slashing", "physical"), filePath, false);
+            for(int i =1 ; i<=15;i++ )
+            {
+                SaveAllItems(CreateItems(i, 80-(i*5), "Bow", filePath, "ranged weapon", 10, "two handed", "piercing", "physical", "shadow weaver"), filePath, false);
+                SaveAllItems(CreateItems(i, 80-(i*5), "Magic Staff", filePath, "magic weapon", 10, "two handed", "bludgeoning", "physical", "all"), filePath, false);
+                SaveAllItems(CreateItems(i, 80-(i*5), "Greatsword", filePath, "melee weapon", 10, "two handed", "slashing", "physical", "fighter"), filePath, false);
+                
+                SaveAllItems(CreateItems(i, 80-(i*5), "Helmet", filePath, "armor", 10, "head", "", "", "all"), filePath, false);
+                SaveAllItems(CreateItems(i, 80-(i*5), "Chestplate", filePath, "armor", 10, "chest", "", "", "all"), filePath, false);
+                SaveAllItems(CreateItems(i, 80-(i*5), "Vembraces", filePath, "armor", 10, "arms", "", "", "all"), filePath, false);
+                SaveAllItems(CreateItems(i, 80-(i*5), "Greaves", filePath, "armor", 10, "legs", "", "", "all"), filePath, false);
+                SaveAllItems(CreateItems(i, 80-(i*5), "Amulet", filePath, "armor", 10, "necklace", "", "", "all"), filePath, false);
+                SaveAllItems(CreateItems(i, 80-(i*5), "Ring", filePath, "armor", 10, "ring", "", "", "all"), filePath, false);
+                SaveAllItems(CreateItems(i, 80-(i*5), "Health Potion", filePath, "potion", 10, "", "", "", "all"), filePath, false);
+            }
 
             CharacterManager manager = new CharacterManager(path);
             manager.RunCharacterMenu();
@@ -74,7 +86,7 @@ namespace CharacterBuilder
             }
         }
 
-        public static List<Item> CreateItems(int itemLevel, int howMany, string itemName, string filePath, string typeOfItem, int weight, string slotType, string attackType, string typeOfDamage)
+        public static List<Item> CreateItems(int itemLevel, int howMany, string itemName, string filePath, string typeOfItem, int weight, string slotType, string attackType, string typeOfDamage, string className)
         {
             List<Item> randomizedItems = new List<Item>();
 
@@ -98,7 +110,19 @@ namespace CharacterBuilder
             {
                 //Equipment as proof of concept for adding to inventory and equipping items
                 List<CharacterClass> c = new List<CharacterClass>();
-                c.Add(newFighter);
+
+                if(className == "fighter")
+                    c.Add(newFighter);
+                if(className == "wizard")
+                    c.Add(newWizard);
+                if(className == "shadow weaver")
+                    c.Add(newShadowWeaver);
+                if(className == "all")
+                {
+                    c.Add(newFighter);
+                    c.Add(newWizard);
+                    c.Add(newShadowWeaver);
+                }
 
                 attackBonusRandom = random.Next(1, itemLevel+upperboundAttack);
                 damageBonusRandom = random.Next(1, itemLevel+upperboundDamage);
@@ -131,13 +155,13 @@ namespace CharacterBuilder
 
                     rangedDamageBonus = (typeOfItem.ToLower() == "ranged weapon") ? damageBonusRandom : 0,
 
-                    magicDamageBonus = (typeOfItem.ToLower() == "magic weapon") ? damageBonusRandom : 0,
+                    magicDamageBonus = (typeOfItem.ToLower() == "magic weapon" || slotType.ToLower()=="ring") ? damageBonusRandom : 0,
 
                     meleeAttackBonus = (typeOfItem.ToLower() == "melee weapon") ? attackBonusRandom :0,
 
                     rangedAttackBonus = (typeOfItem.ToLower() == "ranged weapon") ? attackBonusRandom :0,
 
-                    magicAttackBonus = (typeOfItem.ToLower() == "magic weapon") ? attackBonusRandom :0,
+                    magicAttackBonus = (typeOfItem.ToLower() == "magic weapon"  || slotType.ToLower()=="necklace") ? attackBonusRandom :0,
 
                     armorClassBonus = (typeOfItem.ToLower() == "armor") ? ACBonus :0,
 
@@ -145,13 +169,13 @@ namespace CharacterBuilder
 
                     typeOfDamage = typeOfDamage,
 
-                    strRequirement = (typeOfItem.ToLower() == "melee weapon") ? 15+3*(itemLevel-1): 0,
+                    strRequirement = (typeOfItem.ToLower() == "melee weapon") ? 15+3*(itemLevel-1): (typeOfItem.ToLower() == "armor" && slotType.ToLower() != "necklace" && slotType.ToLower() != "ring") ? 10 + (itemLevel-1): 0,
 
                     dexRequirement = (typeOfItem.ToLower() == "ranged weapon") ? 15+3*(itemLevel-1): 0,
 
-                    wisRequirement = (typeOfItem.ToLower() == "magic weapon") ?  15+3*(itemLevel-1): 0,
+                    wisRequirement = (typeOfItem.ToLower() == "magic weapon" || slotType.ToLower()=="necklace") ?  15+3*(itemLevel-1): 0,
 
-                    magicRequirement = (typeOfItem.ToLower() == "magic weapon") ?  15+3*(itemLevel-1): 0,
+                    magicRequirement = (typeOfItem.ToLower() == "magic weapon"|| slotType.ToLower()=="ring") ?  15+3*(itemLevel-1): 0,
                 };
                 randomizedItems.Add(newItem);
             }
