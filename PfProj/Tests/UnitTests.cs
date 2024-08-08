caa5077ceae81ada76ad771a7a6654c95d849192
ThyPirateKing <pirateking_nick@yahoo.com>
2024-08-08 17:23:19 -0400

    Added Colton's Controllers and API folder PfProj.

---
+++ PfProj/Tests/UnitTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace PfProj.UnitTests
+{
+    [TestFixture(10)]
+    [TestFixture(42)]
+    public class DataDrivenTestFixture
+    {
+        [Test]
+        public void IsNull()
+        {
+            object nada = null;
+
+            // Constraint Syntax
+            Assert.That(nada, Is.Null);
+        }
+    }
+}
